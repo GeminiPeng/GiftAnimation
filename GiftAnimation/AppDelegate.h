dcd686f2994489ec4a90fae23152e8ad1c95524a
abcdpengbo123 <18321507001@163.com>
2016-12-02 09:43:01 +0800

    Initial Commit

---
+++ GiftAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GiftAnimation
+//
+//  Created by Pengbo on 2016/12/2.
+//  Copyright © 2016年 Pengbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
